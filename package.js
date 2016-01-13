6d8e8f69c4614ac957f71a6fa0f6769ab22a2143
Roman <rgnevashev@gmail.com>
2016-01-13 16:07:08 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'rgnevashev:bootstrap-slider',
+  version: '6.0.6',
+  summary: 'a slider element for bootstrap 2/3',
+  git: 'https://github.com/seiyria/bootstrap-slider',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.addFiles([
+    "dist/bootstrap-slider.js",
+    "dist/css/bootstrap-slider.css"
+  ], 'client');
+}); 
